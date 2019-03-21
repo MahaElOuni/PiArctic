2f69c480cb5b8756acb344d8a21e231aa1a84a56
maha <maha.ouni995@gmail.com>
2019-03-21 16:11:39 +0100

    Add Event

--- Web/Models/EventViewModel.cs
+++ Web/Models/EventViewModel.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 
-namespace Domain.Entities
+namespace Web.Models
 {
-    public class Event
+    public class EventViewModel
     {
-        public int EventId { get; set; }
         public String Title { get; set; }
         public String Address { get; set; }
         public int NumberPlaces { get; set; }
@@ -16,11 +14,5 @@ namespace Domain.Entities
         public String Description { get; set; }
         public DateTime Date { get; set; }
         public String OrganizedBy { get; set; }
-        public virtual ICollection<Recommendation> listRecommdendation { get; set; }
-        public virtual ICollection<Ticket> ListTickets { get; set; }
-        public Event()
-        {
-
-        }
     }
 }
