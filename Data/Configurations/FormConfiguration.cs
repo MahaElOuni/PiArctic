210cfa1dc45422b12a86f18eece4662ad81c2287
farouk <farouk.hadjali@esprit.tn>
2019-03-26 16:07:47 +0100

    comiy
    x

---
+++ Data/Configurations/FormConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Configurations
+{
+    class FormConfiguration:EntityTypeConfiguration<Form>
+    {
+        public FormConfiguration()
+        {
+            HasKey(e => new { e.FormId, e.UserId, e.EventId });
+        }
+    }
+}
