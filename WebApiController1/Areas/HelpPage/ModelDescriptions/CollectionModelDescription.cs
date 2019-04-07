28d7fea37044fc0155b122c51941d0a04e32d87e
maha <maha.ouni995@gmail.com>
2019-04-07 02:05:31 +0200

    WebApiController

---
+++ WebApiController1/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebApiController1.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
