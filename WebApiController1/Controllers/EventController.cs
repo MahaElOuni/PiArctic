e20a714e790d5eb2d676239f1cb6fc9f661c6cae
maha <maha.ouni995@gmail.com>
2019-05-08 22:43:27 +0200

    addWebService

--- WebApiController1/Controllers/EventController.cs
+++ WebApiController1/Controllers/EventController.cs
@@ -7,17 +7,16 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace WebAPIController.Controllers
+namespace WebApiController1.Controllers
 {
     public class EventController : ApiController
     {
-
         EventService eventService = new EventService();
 
         // GET: api/Event
+        [HttpGet]
         public IEnumerable<Event> Get()
         {
-
 
             return eventService.GetAll();
         }
@@ -74,29 +73,90 @@ namespace WebAPIController.Controllers
         {
             UserService user = new UserService();
             return user.GetById(id).Email;
-
-
         }
-
-        // GET: api/Event/5
-        public string Get(int id)
+        // POST: api/Event
+        public void PostTask([FromBody]Tasks tasks)
         {
-            return "value";
+            TasksService taskService1 = new TasksService();
+            Tasks t = new Tasks();
+            t.TaskTitle = tasks.TaskTitle;
+            t.UserId = tasks.UserId;
+            t.EventId = tasks.EventId;
+            taskService1.Add(t);
+            taskService1.Commit();
         }
+        // GET: api/Event/5
+        public Event Get(int id)
+        {
+            return eventService.GetById(id);
+        }
+        public int GetOrganizerId(String email)
+        {
+            UserService user = new UserService();
+            return user.GetAll().Where(e => e.Email.equals(email)).Select(e => e.Id);
+                }
 
         // POST: api/Event
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Event eventt)
         {
+            Event e1 = new Event();
+            e1.Title = eventt.Title;
+            e1.Start = eventt.Start;
+            e1.End = eventt.End;
+            eventService.Add(e1);
+            eventService.Commit();
         }
 
         // PUT: api/Event/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]Event eventt)
         {
+            Event e = eventService.GetById(id);
+            e.Title = eventt.Title;
+            e.Description = eventt.Description;
+            e.Start = eventt.Start;
+            e.End = eventt.End;
+            e.IsFullDay = eventt.IsFullDay;
+            e.OrganizedBy = eventt.OrganizedBy;
+            e.Price = eventt.Price;
+            e.Slogan = eventt.Slogan;
+            e.NumberPlaces = eventt.NumberPlaces;
+            e.ThemeColor = eventt.ThemeColor;
+            eventService.Update(e);
+            eventService.Commit();
         }
 
         // DELETE: api/Event/5
         public void Delete(int id)
         {
+            eventService.Delete(eventService.GetById(id));
+            eventService.Commit();
+        }
+        public List<Tasks> OrganizerTasks(int eventId, int organizerId)
+        {
+            Event e = Get(eventId);
+            List<Tasks> tasks = new List<Tasks>();
+            foreach (Tasks t in e.ListTask)
+            {
+                if (t.UserId == organizerId)
+                {
+                    tasks.Add(t);
+                }
+            }
+            return tasks;
+        }
+        public IEnumerable<Event> OrganizerEvents(int organizerId)
+        {
+            List<Event> e = new List<Event>();
+
+            TasksService taskService = new TasksService();
+            var eventsId = taskService.GetMany().Where(o => o.UserId == organizerId).Select(o => o.EventId).Distinct();
+            foreach (var i in eventsId)
+            {
+                e.Add(this.Get(i.Value));
+
+            }
+            return e;
+
         }
 
         [HttpGet]
