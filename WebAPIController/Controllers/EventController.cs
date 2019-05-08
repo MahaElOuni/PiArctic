d95039f827a1adfdfd644679bb40edda6eff5d1d
maha <maha.ouni995@gmail.com>
2019-05-08 02:57:49 +0200

    ModificationWebController

---
+++ WebAPIController/Controllers/EventController.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+using Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIController.Controllers
+{
+    public class EventController : ApiController
+    {
+        EventService e = new EventService();
+        // GET: api/Event
+        public IEnumerable<Event> Get()
+        {
+            return e.GetAll();
+        }
+
+        // GET: api/Event/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Event
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Event/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Event/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
